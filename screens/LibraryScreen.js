0f84e51d61a92a8b3399832bc695d7c73aeee4c2
gabtorre <gtorresx3@gmail.com>
2020-12-28 14:25:06 -0800

    Refactor Library

--- screens/LibraryScreen.js
+++ screens/LibraryScreen.js
@@ -1,81 +1,19 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 import Firebase, { db } from '../firebase';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
+import WatchList from '../components/Library/WatchList';
+import WatchedList from '../components/Library/WatchedList';
 
-export default function HomeScreen() {
+export default function LibraryScreen() {
 
   const auth = Firebase.auth();
   const currentUser = auth.currentUser.uid;
-  const [movies] = useDocumentData(db.doc('users/' + currentUser));
 
   return (
-    <SafeAreaView style={styles.container}>
-    <Text style={styles.header}>Watch List</Text>
-    <ScrollView horizontal bounces style={styles.scrollView}>
-      {movies && movies.watchlist.map(movie =>
-        <View style={styles.movieDiv}>
-          <Image
-          style={styles.image}
-          source={{
-            uri: `https://image.tmdb.org/t/p/w500/${movie.poster}`,
-          }}
-          />
-          <Text key={movie.movieid} style={styles.title}>{movie.title}</Text>
-        </View>
-      )}
+    <ScrollView bounces={false}>
+      <WatchList key="watchlist" />
+      <WatchedList key="watchedlist" />
     </ScrollView>
-    <Text style={styles.header}>Watched List</Text>
-    <ScrollView horizontal bounces style={styles.scrollView}>
-      {movies && movies.watched.map(movie =>
-        <View style={styles.movieDiv}>
-          <Image
-          style={styles.image}
-          source={{
-            uri: `https://image.tmdb.org/t/p/w500/${movie.poster}`,
-          }}
-          />
-          <Text key={movie.movieid} style={styles.title}>{movie.title}</Text>
-        </View>
-      )}
-    </ScrollView>
-    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: '#181D2F',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: '700',
-  },
-  movieDiv: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginRight: 20,
-  },
-  image: {
-    width: 150,
-    height: 225,
-    padding: 10,
-    borderRadius: 25,
-  },
-  title: {
-    color: '#ffffff',
-    textAlign: 'center',
-    marginTop: 10,
-    width: 150,
-  },
-  scrollView: {
-    flex: 1,
-    marginHorizontal: 20,
-  },
-});
