ecdd1d3685333111ab60722e7f4d282b60abde5a
Beatrix Woo <beatrixwho@gmail.com>
2020-12-28 14:36:18 -0800

    add profile to setting

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Firebase, { db } from '../firebase';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
 
 export default function SettingsScreen() {
+
+  const auth = Firebase.auth();
+  const currentUser = auth.currentUser;
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.profilepic}
+        resizeMode={"cover"}
+        source={{
+          uri: currentUser.photoURL
+        }}/>
+      <Text style={styles.text}>{currentUser.displayName}</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#181D2F',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profilepic: {
+    width: 100,
+    height: 100
+  },
+  text: {
+    color: 'white',
+  }
+});
